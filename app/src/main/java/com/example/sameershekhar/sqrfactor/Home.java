cbf27484f8e74a9dcf95c3025c73937df5510f85
sameer472 <sameer@hebecollins.com>
2018-06-13 13:02:49 +0530

    First Commit

---
+++ app/src/main/java/com/example/sameershekhar/sqrfactor/Home.java
@@ -0,0 +1,80 @@
+package com.example.sameershekhar.sqrfactor;
+
+import android.arch.core.executor.TaskExecutor;
+import android.content.Intent;
+import android.os.TestLooperManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+public class Home extends AppCompatActivity {
+    private TextView name;
+    private Button signOut;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        name = findViewById(R.id.name);
+        signOut=findViewById(R.id.sign_out);
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        name.setText("Hello "+ user.getEmail());
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(Home.this, LoginActivity.class));
+                    finish();
+                }
+
+            }
+        };
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+    }
+
+    public void signOut() {
+        auth.signOut();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+}
